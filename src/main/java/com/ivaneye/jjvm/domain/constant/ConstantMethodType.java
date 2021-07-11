569fa088bbf6b10f392e79620bfe039f14863667
王一帆 <wang.yifan4@iwhalecloud.com>
2021-07-11 10:53:34 +0800

    - 添加Method常量

--- src/main/java/com/ivaneye/jjvm/domain/constant/ConstantMethodType.java
+++ src/main/java/com/ivaneye/jjvm/domain/constant/ConstantMethodType.java
@@ -8,36 +8,32 @@ import com.ivaneye.jjvm.domain.type.U2;
  * @author ivaneye
  * @since 2021-07-10 22:45:09
  */
-public class ConstantNameAndType implements Constant {
+public class ConstantMethodType implements Constant {
 
     private U1 tag;
-    private U2 nameIndex;
     private U2 descIndex;
     private ClassInfo classInfo;
 
-    public ConstantNameAndType(U1 tag, U2 nameIndex, U2 descIndex, ClassInfo classInfo) {
+    public ConstantMethodType(U1 tag, U2 descIndex, ClassInfo classInfo) {
         this.tag = tag;
-        this.nameIndex = nameIndex;
         this.descIndex = descIndex;
         this.classInfo = classInfo;
     }
 
     @Override
     public String type() {
-        return "NameAndType";
+        return "MethodType";
     }
 
     @Override
     public String value() {
-        return classInfo.getConstantPool().get(nameIndex.toInt()).value()
-                + ":"
-                + classInfo.getConstantPool().get(descIndex.toInt()).value();
+        return classInfo.getConstantPool().get(descIndex.toInt()).value();
     }
 
     @Override
     public String toString() {
         return type()
-                + "    #" + nameIndex.toInt() + ":#" + descIndex.toInt()
+                + "    #" + descIndex.toInt()
                 + "    // " + value();
     }
 }
